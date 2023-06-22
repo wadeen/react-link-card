1396a8fe50f83fc68130e44c46b6715fb4adccc6
wadeen <meister.vvdo@gmail.com>
2023-06-22 21:35:57 +0900

    🚧 WIP: Layout and Composition

---
+++ src/components/LinkCard/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./index.module.css";
+type LinkCardProps = {};
+
+const LinkCard: React.FC<LinkCardProps> = ({}) => {
+  return (
+    <div className={styles.wrapper}>
+      <p>あああ</p>
+    </div>
+  );
+};
+
+export default LinkCard;
