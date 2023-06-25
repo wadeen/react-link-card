25dc2f7348e22dc1694647d562561c408a6e7bcf
wadeen <meister.vvdo@gmail.com>
2023-06-25 17:35:44 +0900

    ✨ Add: sample component, base style

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
 import styles from "./App.module.css";
 import { LinkCard, LinkCardLarge } from "./components/LinkCard";
-import useFetchLinkData, { FetchLinkUrlType } from "./components/hooks/useFetchLinkData";
+// import useFetchLinkData, { FetchLinkUrlType } from "./components/hooks/useFetchLinkData";
 
 function App() {
   return (
     <div className={styles.wrapper}>
-      <h1>Demo page</h1>
+      <h1 style={{ textAlign: "center" }}>Demo page</h1>
       <div className={styles.container}>
         {/* サンプルの利用（標準・横並び） */}
-        <LinkCard url={"https://zenn.dev/wadeen"} target="_blank" />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+          }}
+        >
+          <LinkCard url={"https://wadeen.net"} titleTagName="h3" />
+          <LinkCard url={"https://zenn.dev/wadeen"} />
+          <LinkCard url={"https://zenn.dev/chot/articles/ddd2844ad3ae61"} />
+          <LinkCard url={"https://obake.land/blog/20230216"} target="_blank" />
+        </div>
 
         {/* サンプルの利用（大・縦並び） */}
-        <LinkCardLarge url={"https://zenn.dev/wadeen"} />
-
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+          }}
+        >
+          <LinkCardLarge url={"https://wadeen.net"} titleTagName="h3" />
+          <LinkCardLarge url={"https://zenn.dev/wadeen"} />
+          <LinkCardLarge url={"https://zenn.dev/chot/articles/ddd2844ad3ae61"} />
+          <LinkCardLarge url={"https://obake.land/blog/20230216"} target="_blank" />
+        </div>
         {/* カスタムフックの利用 */}
-        <CardItem url={"https://wadeen.net"} />
+        {/* <CardItem url={"https://wadeen.net"} /> */}
       </div>
     </div>
   );
@@ -26,18 +47,18 @@ export default App;
   /* カスタムフックの利用 */
 }
 
-const CardItem = ({ url }: FetchLinkUrlType) => {
-  const { loading, error, data: linkCardData } = useFetchLinkData(url);
+// const CardItem = ({ url }: FetchLinkUrlType) => {
+//   const { loading, error, data: linkCardData } = useFetchLinkData(url);
 
-  if (error) return <p>ERROR!</p>;
-  if (loading) return <p>loading...</p>;
+//   if (error) return <p>ERROR!</p>;
+//   if (loading) return <p>loading...</p>;
 
-  return (
-    <a href={url} target="_blank">
-      <h2>{linkCardData?.title}</h2>
-      <img src={linkCardData?.ogp} alt="" />
-      <img src={linkCardData?.favicon} alt="" />
-      <p>{linkCardData?.description}</p>
-    </a>
-  );
-};
+//   return (
+//     <a href={url} target="_blank">
+//       <h2>{linkCardData?.title}</h2>
+//       <img src={linkCardData?.ogp} alt="" />
+//       <img src={linkCardData?.favicon} alt="" />
+//       <p>{linkCardData?.description}</p>
+//     </a>
+//   );
+// };
--- src/components/LinkCard/index.tsx
+++ src/components/LinkCard/index.tsx
@@ -1,38 +1,56 @@
 import { AnchorHTMLAttributes } from "react";
 import useFetchLinkData, { FetchLinkUrlType } from "../hooks/useFetchLinkData";
+import styles from "./index.module.css";
 
 type LinkCardProps = {
+  /**  */
   target?: AnchorHTMLAttributes<HTMLAnchorElement>["target"];
+  /**  */
+  titleTagName?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 } & FetchLinkUrlType;
 
-export const LinkCard = ({ url, target = "_blank" }: LinkCardProps) => {
+export const LinkCard = ({ url, target = "_blank", titleTagName = "h2" }: LinkCardProps) => {
   const { loading, error, data: linkCardData } = useFetchLinkData(url);
 
   if (error) return <p>ERROR!</p>;
   if (loading) return <p>loading...</p>;
 
+  const Tag = titleTagName;
+
   return (
-    <a href={url} target={target}>
-      <h2>{linkCardData?.title}</h2>
-      <img src={linkCardData?.ogp} alt="" />
-      <img src={linkCardData?.favicon} alt="" />
-      <p>{linkCardData?.description}</p>
+    <a href={url} target={target} className={styles.baseContainer}>
+      <div className={styles.baseTextArea}>
+        <div className={styles.baseTitleArea}>{linkCardData?.title && <Tag className={styles.baseTitle}>{linkCardData?.title}</Tag>}</div>
+        {linkCardData?.description && (
+          <div className={styles.baseDescArea}>
+            {linkCardData?.favicon && <img src={linkCardData?.favicon} alt="" width={24} height={24} />}
+            <p className={styles.baseText}>{linkCardData?.description}</p>
+          </div>
+        )}
+      </div>
+      {linkCardData?.ogp && <img src={linkCardData?.ogp} alt="" className={styles.baseImg} />}
     </a>
   );
 };
 
-export const LinkCardLarge = ({ url, target }: LinkCardProps) => {
+export const LinkCardLarge = ({ url, target = "_blank", titleTagName = "h2" }: LinkCardProps) => {
   const { loading, error, data: linkCardData } = useFetchLinkData(url);
 
   if (error) return <p>ERROR!</p>;
   if (loading) return <p>loading...</p>;
 
+  const Tag = titleTagName;
+
   return (
-    <a href={url} target={target}>
-      <h2>{linkCardData?.title}</h2>
-      <img src={linkCardData?.ogp} alt="" />
+    <a href={url} target={target} className={styles.container}>
+      <div className={styles.textArea}>
+        <Tag className={styles.title}>{linkCardData?.title}</Tag>
+        <div className={styles.text}>
+          <img src={linkCardData?.ogp} alt="" className={styles.imgStyle} width={24} height={24} />
+          <p>{linkCardData?.description}</p>
+        </div>
+      </div>
       <img src={linkCardData?.favicon} alt="" />
-      <p>{linkCardData?.description}</p>
     </a>
   );
 };
